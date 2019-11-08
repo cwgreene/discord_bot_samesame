f01d77797f5a39ec3bc09d01f3aec5601dc498cd
Chris Greene <archgoon@gmail.com>
2019-11-08 00:22:51 -0800

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import supersecret
+
+import samesame
+
+BOT_TOKEN = supersecret.getSecret('discord_bot_samesame', 'bot_token')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.content.startswith("!samesame") and message.author != "samesame#1172":
+            text = message.content[len("!samesame"):]
+            result = await message.channel.send(samesame.samesame(text))
+        else:
+            print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(BOT_TOKEN)
