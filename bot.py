f209bd56b333754c74db89434bfd93fb834a4b47
Chris Greene <archgoon@gmail.com>
2020-02-23 10:14:52 -0800

    Print messages. probably remove.

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.content.startswith("!samesame") and message.author != "samesame#1172":
             text = message.content[len("!samesame"):]
+            print('Message from {0.author}: {0.content}'.format(message))
             result = await message.channel.send(samesame.samesame(text))
         else:
             print('Message from {0.author}: {0.content}'.format(message))
